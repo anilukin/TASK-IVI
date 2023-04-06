aca03c22f284b37f0d3336f0824b300d6832b5e0
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2023-04-06 18:21:33 +0200

    add search filter

--- src/Components/PersonsList.jsx
+++ src/Components/PersonsList.jsx
@@ -5,9 +5,26 @@ import React from 'react';
 export function PersonsList({ list }) {
   const [sortedKey, setSortedKey] = useState(null);
   const [sortDirection, setSortDirection] = useState('inc');
+  const [filterExpr, setFilterExpr] = useState('')
+
   const keys = Object.keys(list[0]);
 
-  const sortedList = [...list];
+  let sortedList = [...list];
+
+  if (filterExpr) {
+    const isContainsExpr = (item) => {
+      const valuesArr = Object.values(item);
+      for (let i = 0; i < valuesArr.length; i += 1) {
+        const normalizedVal = valuesArr[i].toString().toLowerCase()
+        if (normalizedVal.includes(filterExpr)) {
+          return true;
+        };
+      }
+      return false;
+    };
+    sortedList = sortedList.filter((item) => isContainsExpr(item));
+  }
+
   if (sortedKey) {
     switch (sortDirection) {
       case 'inc':
@@ -41,19 +58,22 @@ export function PersonsList({ list }) {
   };
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          {keys.map((key) => <th key={key} onClick={() => { setStates(key) }}>{key}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {sortedList.map((item, ind) => (
-          <tr key={`tr-${ind}`}>
-            {keys.map((key, i) => <td key={`td-${ind}-${i}`}>{item[key]}</td>)}
+    <>
+      <input onChange={(e) => setFilterExpr(e.target.value.toLowerCase())} />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            {keys.map((key) => <th key={key} onClick={() => { setStates(key) }}>{key}</th>)}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {sortedList.map((item, ind) => (
+            <tr key={`tr-${ind}`}>
+              {keys.map((key, i) => <td key={`td-${ind}-${i}`}>{item[key]}</td>)}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 }
